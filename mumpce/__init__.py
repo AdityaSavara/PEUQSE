f00773bf9ed97b61aa086cee6e3ecf0ef0baee49
AdityaSavara <39929571+AdityaSavara@users.noreply.github.com>
2019-12-04 14:16:11 -0500

    adding mumpce directory

---
+++ mumpce/__init__.py
@@ -0,0 +1,10 @@
+### mumpce init script
+import sys
+import os
+
+sys.path.append(os.path.dirname(__file__))
+
+from Project import *
+from measurement import tqfunc
+#from response_surface import response_surface
+#from solution import solution
