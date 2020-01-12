f25d711162d17ad3a3dfc4b1c16c5211c5f63e5e
AdityaSavara <39929571+AdityaSavara@users.noreply.github.com>
2020-01-12 16:57:40 -0500

    using ability to define number of ticks and font

--- mumpce_custom_plotting_example.py
+++ mumpce_custom_plotting_example.py
@@ -11,11 +11,9 @@ import mumpce.solution as mumpceSolution
 import numpy as np
 import matplotlib
 from matplotlib import cm #EAW 2020/01/07
-matplotlib.use('Agg') #EAW 2020/01/07
+#matplotlib.use('Agg') #EAW 2020/01/07
 
-#TODO: Make 2D parameter response surfaces like in the perspective figures. Can make it for each variable pair. Should have it as an option in the UserInput as True, False, or a list of pairs for which ones to make.
-#Below is pseudo code to begin doing that. See more info at https://github.com/AdityaSavara/ODE-KIN-BAYES-SG-EW/issues/9
-#active_params= ['a']
+###THE BELOW LINES OF CODE SETUP THE DATA IN THE MUMPCE PROJECT OBJECT####
 mumpceProjectObject = mumpceProject.Project()
 mumpceProjectObject.model_parameter_info = np.array([{'parameter_number': 0, 'parameter_name': 'Parameter 0', 'parameter_value': 1.0},
  {'parameter_number': 1, 'parameter_name': 'Parameter 1', 'parameter_value': 1.0}, #The parameter values are not used in making these contour plots.
@@ -40,41 +38,36 @@ mumpceSolutionsObject = mumpceSolution.Solution(Posterior_mu_vector, Posterior_c
 mumpceProjectObject.solution = mumpceSolutionsObject
 mumpceProjectObject.pairsOfParameterIndices = [[0, 1], [1, 2],[3, 4]]
 
-#np.array([[ 0.0148872,-0.01894579, -0.01047339,0.01325883,0.04734254],
-# [-0.01894579,0.04284732, -0.00131389, -0.04801795, -0.04545703],
-# [-0.01047339, -0.00131389,0.02343653,0.01588293, -0.05618226],
-# [ 0.01325883, -0.04801795,0.01588293,0.08171972,0.00875017],
-# [ 0.04734254, -0.04545703, -0.05618226,0.00875017,0.20669273]])
 
-#This makes the figures as originally programmed. It assumes/requries things be normalized to 1.
-mumpceProjectObject.solution.figure_name='mumpce_plots_alpha'
-#mumpceProjectObject.solution.num_pts_per_axis = 500
+###THE BELOW LINES OF CODE MAKE EXAMPLE PLOTS WITH THE DEFAULT AND WITH THE NEW REVISED WAY####
+
+#This makes the figures as originally programmed in mumpce, which assumes/requries the cov to be normalized to 1.
 mumpceProjectObject.plot_pdfs(mumpceProjectObject.pairsOfParameterIndices)
-cmap = cm.Reds
-
 
 #I have expanded the code to allow more versatility an optional argument called contour_settings_custom.
-#  It does not assume/require that things be normalized to 1, but requires the below variables to be populated.
-mumpceProjectObject.solution.contour_axis_range = np.array([[-3.0,1.5],[-1.5,3.0],[-1.5,1.5],[-1.5,1.5],[-1,1],[-1,1],[-1,1]]) #this must be made into a numpy array.
-mumpceProjectObject.solution.contour_resolution = np.array([0.01,0.01,0.01,0.01,0.01,0.01])
-mumpceProjectObject.solution.axis_tick_spacing = np.array([1,1.5,1,1,1,1])
-mumpceProjectObject.solution.contour_levels = np.exp((np.arange(-2,0,0.5) ** 2) * -1)
-mumpceProjectObject.solution.figure_name='mumpce_plots_beta'
-mumpceProjectObject.plot_pdfs(mumpceProjectObject.pairsOfParameterIndices, contour_settings_custom = {"colored":True})
-
-mumpceProjectObject.solution.figure_name='mumpce_plots_beta2'
-contour_settings_custom = {}
-contour_settings_custom['colored'] = True
-contour_settings_custom['colorbars'] = False
+#It does not assume/require that things be normalized to 1.
+contour_settings_custom={}
+contour_settings_custom['figure_name']='mumpce_plots_02'
 mumpceProjectObject.plot_pdfs(mumpceProjectObject.pairsOfParameterIndices, contour_settings_custom = contour_settings_custom)
 
-#Here is the graph after Eric has made the needed changes to have contour_settings_auto....
-#Right now it produces fairly similar looking graphs. Later, it will change things more!
-mumpceProjectObject.solution.figure_name='mumpce_plots_gamma'
 contour_settings_custom = {}
-contour_settings_custom['colored'] = True
+contour_settings_custom['figure_name']='mumpce_plots_03'
 contour_settings_custom['colormap_posterior_customized'] = "Oranges"
 contour_settings_custom['colormap_prior_customized'] = "Greens"
 contour_settings_custom['contours_normalized'] = False
 contour_settings_custom['center_on'] = 'prior'
+mumpceProjectObject.plot_pdfs(mumpceProjectObject.pairsOfParameterIndices, contour_settings_custom = contour_settings_custom)
+
+
+contour_settings_custom = {}
+contour_settings_custom['figure_name']='mumpce_plots_04'
+contour_settings_custom['colorbars'] = False
+mumpceProjectObject.plot_pdfs(mumpceProjectObject.pairsOfParameterIndices, contour_settings_custom = contour_settings_custom)
+
+contour_settings_custom = {}
+contour_settings_custom['figure_name']='mumpce_plots_05'
+contour_settings_custom['fontsize'] = 20
+contour_settings_custom['num_x_ticks'] = 4
+contour_settings_custom['num_y_ticks'] = 4
+contour_settings_custom['colorbars'] = False
 mumpceProjectObject.plot_pdfs(mumpceProjectObject.pairsOfParameterIndices, contour_settings_custom = contour_settings_custom)
