5f4fa36ab59a1ee9b4e6c635cf55dd2c84d33168
AdityaSavara <39929571+AdityaSavara@users.noreply.github.com>
2020-10-19 03:49:50 -0400

    Fixed multiple responses counting and plotting
    It was a much bigger hassle than expected. I could not get the plotting to work for Example07f without making a few special if statements in the code. I am not very happy with the current solution, but so far it is working. It may need to be changed again once there is a 'better' multiple response example.

--- Examples/Example07/processing_function_two_response.py
+++ Examples/Example07/processing_function_two_response.py
@@ -12,3 +12,21 @@ def split_to_separated_lists(input):
     for element in input:
         listAfterSplit.append([element]) #Note the extra bracket inside to create nesting.
     return listAfterSplit
+
+
+def split_to_separated_lists_plus_1h(input):
+    listAfterSplit = []
+    for element in input:
+        listAfterSplit.append([element]) #Note the extra bracket inside to create nesting.
+    listAfterSplitPlus1 = listAfterSplit #initializing new variable name.
+    listAfterSplitPlus1[1].append(0)
+    return listAfterSplitPlus1
+
+
+def split_to_separated_lists_plus_1i(input):
+    listAfterSplit = []
+    for element in input:
+        listAfterSplit.append([element]) #Note the extra bracket inside to create nesting.
+    listAfterSplitPlus1 = listAfterSplit #initializing new variable name.
+    listAfterSplitPlus1[0].append(0)
+    return listAfterSplitPlus1
