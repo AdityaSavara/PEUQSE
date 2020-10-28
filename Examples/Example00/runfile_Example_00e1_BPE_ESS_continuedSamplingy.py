6ed07bc27556529a4e60d94eeee7fb41065eeec4
AdityaSavara <39929571+AdityaSavara@users.noreply.github.com>
2020-10-28 10:54:31 -0400

    Renamed some files etc.    Continued sampling with multistart not working yet

--- Examples/Example00/runfile_Example_00e1_BPE_ESS_continuedSamplingy.py
+++ Examples/Example00/runfile_Example_00e1_BPE_ESS_continuedSamplingy.py
@@ -1,18 +1,19 @@
-import sys; sys.path.append('../../'); import CheKiPEUQ as CKPQ
+import sys; sys.path.append('../../');  import CheKiPEUQ as CKPQ
 import CheKiPEUQ.UserInput as UserInput
 
 if __name__ == "__main__":
-    import simulation_model_00
-    
-    
-    
-    UserInput.responses['responses_abscissa'] = [600,1100,1400]    
-    UserInput.responses['responses_observed'] = [360500, 580500, 1620500]
-    UserInput.responses['responses_observed_uncertainties'] = [200000, 300000, 200000]    
+    import observed_values_00  #Just a simple example. The user can also put the values in directly into the runfile or extract from a csv, for example.
+    import simulation_model_00 #Simple example.
+        
+    UserInput.responses['responses_abscissa'] = observed_values_00.observed_data_x_values
+    UserInput.responses['responses_observed'] = observed_values_00.observed_data_y_values
+    UserInput.responses['responses_observed_uncertainties'] = observed_values_00.observed_data_y_values_uncertainties
+
     
     UserInput.simulated_response_plot_settings['x_label'] = 'distance (m)'
     UserInput.simulated_response_plot_settings['y_label'] = r'$time (s)$'
     UserInput.simulated_response_plot_settings['fontdict'] = {'size':16}
+    
     
     UserInput.model['parameterNamesAndMathTypeExpressionsDict'] = {'a':'a','b':'b'}
     UserInput.model['InputParameterPriorValues'] = [200, 500] #prior expected values for a and b
@@ -21,24 +22,16 @@ if __name__ == "__main__":
 
     
     UserInput.model['simulateByInputParametersOnlyFunction'] = simulation_model_00.simulation_function_wrapper #This must simulate with *only* the parameters listed above, and no other arguments.
-    simulation_model_00.x_values_for_data = UserInput.responses['responses_abscissa']  #Setting the x_values_for_data inthe simulation module.
 
-    UserInput.parameter_estimation_settings['mcmc_length'] = 100
     
+    UserInput.parameter_estimation_settings['mcmc_threshold_filter_samples'] = True
+
     UserInput.parameter_estimation_settings['mcmc_random_seed'] = 0
-    UserInput.parameter_estimation_settings['mcmc_parallel_sampling'] = False
-    UserInput.parameter_estimation_settings['multistart_parallel_sampling'] = True
-    UserInput.parameter_estimation_settings['mcmc_exportLog'] = True
-    
-    
-
-
     #After making the UserInput, now we make a 'parameter_estimation' object from it.
     PE_object = CKPQ.parameter_estimation(UserInput)
-    PE_object.doMultiStart('doMetropolisHastings', initialPointsDistributionType='grid')
+    PE_object.doEnsembleSliceSampling()
+    PE_object.doEnsembleSliceSampling()
     PE_object.createAllPlots() #This function calls each of the below functions so that the user does not have to.
 #    PE_object.makeHistogramsForEachParameter()    
 #    PE_object.makeSamplingScatterMatrixPlot()
 #    PE_object.createSimulatedResponsesPlot()
-
-    
