3a0dfe0cf927608e59d9d534f6bb95f55940ce76
AdityaSavara <39929571+AdityaSavara@users.noreply.github.com>
2021-04-18 14:05:17 -0400

    Printing a message at start of permutations search, if verbose.

--- CheKiPEUQ/InverseProblem.py
+++ CheKiPEUQ/InverseProblem.py
@@ -617,6 +617,8 @@ class parameter_estimation:
             searchType = 'getLogP'
         self.permutation_searchType = searchType #This is mainly for consolidate_parallel_sampling_data
         verbose = self.UserInput.parameter_estimation_settings['verbose']
+        if verbose:
+            print("Starting multistart/permutations search.")
         file_name_prefix, file_name_suffix, directory_name_suffix = self.getParallelProcessingPrefixAndSuffix() #As of Nov 21st 2020, these should always be '' since multiStart_continueSampling is not intended to be used with parallel sampling.
         if self.UserInput.parameter_estimation_settings['mcmc_continueSampling']  == 'auto':
             mcmc_continueSampling = False #need to set this variable to false if it's an auto. The only time mcmc_continue sampling should be on for multistart is if someone is doing it intentionally, which would normally only be during an MPI case.                                                                                            
