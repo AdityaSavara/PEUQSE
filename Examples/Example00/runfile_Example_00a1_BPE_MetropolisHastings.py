da4de196556631d468f03e2344611c4700508493
AdityaSavara <39929571+AdityaSavara@users.noreply.github.com>
2021-04-19 12:14:24 -0400

    Update runfile_Example_00a1_BPE_MetropolisHastings.py

--- Examples/Example00/runfile_Example_00a1_BPE_MetropolisHastings.py
+++ Examples/Example00/runfile_Example_00a1_BPE_MetropolisHastings.py
@@ -23,6 +23,7 @@ if __name__ == "__main__":
     
     UserInput.model['simulateByInputParametersOnlyFunction'] = simulation_model_00.simulation_function_wrapper #This must simulate with *only* the parameters listed above, and no other arguments.
 
+    UserInput.parameter_estimation_settings['mcmc_checkPointFrequency'] = 1000
     UserInput.parameter_estimation_settings['mcmc_length'] = 10000 #10000 is the default.
 
 
@@ -30,13 +31,21 @@ if __name__ == "__main__":
     #After making the UserInput, now we make a 'parameter_estimation' object from it.
     PE_object = CKPQ.parameter_estimation(UserInput)
     PE_object.doMetropolisHastings()
-    PE_object.createAllPlots() #This function calls each of the below functions so that the user does not have to.
-#    PE_object.makeHistogramsForEachParameter()    
-#    PE_object.makeSamplingScatterMatrixPlot()
-#    PE_object.createSimulatedResponsesPlots()
+    
+    
+    ####NORMALLY WE WOULD NOW CREATE ALL PLOTS, BUT IN THIS EXAMPLE WE WILL DELAY DOING SO IN ORDER TO SHOW HOW TO SAVE AND LOAD A PE_OBJECT TO AND FROM A FILE.
+    #PE_object.createAllPlots()
 
     #to save a PE_object for later
     PE_object.save_to_dill("PE_object_00a1")
     
-    #to load a PE_object after some time, can be done from another file
-    PE_object.load_from_dill("PE_object_00a1")
+    #to load a PE_object after some time, first one has to put (any) UserInput to create a PE_object, then to load from file.
+    #these two steps can be done in a different python file. A different PE_object name is being used to emphasize that this process can be done from a different python file.
+    PE_object2 = CKPQ.parameter_estimation(UserInput)
+    PE_object2 = PE_object2.load_from_dill("PE_object_00a1")
+    
+    
+    PE_object2.createAllPlots() #This function calls each of the below functions so that the user does not have to.
+#    PE_object.makeHistogramsForEachParameter()    
+#    PE_object.makeSamplingScatterMatrixPlot()
+#    PE_object.createSimulatedResponsesPlots()
