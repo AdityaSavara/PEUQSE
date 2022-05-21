58102162affb9c1ecc5634940dc54bb2ccb5ab94
AdityaSavara <39929571+AdityaSavara@users.noreply.github.com>
2022-05-21 00:48:07 -0400

    4.0 Plotting CTI output with new canteraKineticsParser and plotting versus old outputs
    The outputs are slightly different, but seem like numerical errors and seem okay.

--- PEUQSE/simulationDriver_YAML/example_runfile_yaml.py
+++ PEUQSE/simulationDriver_YAML/example_runfile_yaml.py
@@ -41,7 +41,7 @@ def main():
         canteraSimulate.create_yaml_and_SimulatePFRorTPRwithCantera("ceO2", modified_reactions_parameters_array, ceO2_input_simulation_settings, yaml_top_info_string = yaml_top_info_string)
         np.savetxt("ceO2_output_rates_all_"+str(multiplyingFactor)+".csv", rates_all_array, delimiter=",", comments='', header=rates_all_array_header)
         print("round", multiplyingFactor, "Simulation Finished")
-        
+    sys.exit()    
     print("####EXAMPLE 3#####")
     #This is like example 2, only now we use the modifyAllAdjustableReactionParametersArrayValues function rather than sticking values in by simple index.
     model_name = "ceO2"
