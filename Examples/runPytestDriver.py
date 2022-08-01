c03e7622bbd64df96f2a1ae19dbdd3a42adc179d
AdityaSavara <39929571+AdityaSavara@users.noreply.github.com>
2022-08-01 13:24:11 -0400

    Update runPytestDriver.py

--- Examples/runPytestDriver.py
+++ Examples/runPytestDriver.py
@@ -1,3 +1,14 @@
 import UnitTesterSG
 
-UnitTesterSG.pytestDriver()
+import sys
+
+#We want runPytestDriver.py to give a pytest exitCode of 1 for travis CI purposes. That means we need to create an "actual" error when any test fails.
+#The pytestDriver function of UnitTesterSG has an option to 'crash' with an error if "failWithError" is passed in.
+#So the below lines of code are such that if the command line has 'python runPytestDriver.py failWithError"
+if (len(sys.argv) <= 1):
+    UnitTesterSG.pytestDriver(failWithError=False)
+elif sys.argv[1].strip() == '':    
+    UnitTesterSG.pytestDriver(failWithError=False)
+elif sys.argv[1].strip().lower() == 'failwitherror':
+    sys.argv.pop(1) #Need to remove the argument from sys.argv, because pytest also looks in the system arguments and if "failWithError" is still present, that will cause an undesired error.
+    UnitTesterSG.pytestDriver(failWithError=True)
