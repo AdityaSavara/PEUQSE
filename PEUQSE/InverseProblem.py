663978d63bd1919010824026458c408ae14988ae
AdityaSavara <39929571+AdityaSavara@users.noreply.github.com>
2022-08-03 16:47:10 -0400

    doSinglePoint and reducedParameter space

--- PEUQSE/InverseProblem.py
+++ PEUQSE/InverseProblem.py
@@ -1704,6 +1704,9 @@ class parameter_estimation:
         #objectiveFunction can be 'logP' or 'SSR'
         if type(discreteParameterVector)==type(None): #If somebody did not feed a specific vector, we take the initial guess.
             discreteParameterVector = self.UserInput.InputParameterInitialGuess
+        if self.reducedParameterSpaceOn: #if reduced parameter space is on, we need to use a reduced discreteParameterVector
+            reducedIndices = UserInput.model['reducedParameterSpace']
+            discreteParameterVector = returnReducedIterable(discreteParameterVector)
         if objectiveFunction=='logP':
             self.map_parameter_set = discreteParameterVector
             self.map_logP = self.getLogP(discreteParameterVector)
