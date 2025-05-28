271a78f2a89883236f2dd7ee1e2516e1cf387bfe
Aditya Savara <39929571+AdityaSavara@users.noreply.github.com>
2025-05-28 02:19:50 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import find_packages, setup, Command
 NAME = 'PEUQSE'
 DESCRIPTION = 'This is the PEUQSE package which stands for Parameter Estimation and Uncertainty Quantification for Science and Engineering. See https://github.com/AdityaSavara/PEUQSE for more information.'
 URL = 'https://github.com/AdityaSavara/PEUQSE'
-EMAIL = 'AditySavara2008@u.northwestern.edu'  #Eric A. Walker is a co-author and can be reached at WALKER, ERIC <walke299@email.sc.edu>
+EMAIL = 'AdityaSavara2008@u.northwestern.edu'  #Eric A. Walker is a co-author and can be reached at WALKER, ERIC <walke299@email.sc.edu>
 AUTHOR = 'Aditya Savara'
 REQUIRES_PYTHON = '>=3.5.0'
 
